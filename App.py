35723686600cfd5440d06181a5dc150c31000857
CR4CKKER <76111583+cr4ckker@users.noreply.github.com>
2022-11-28 11:30:02 +0300

    Added wheels and second player
    Added wheels, possibility to add more cars. Slightly optimized CreateMove

--- App.py
+++ App.py
@@ -8,9 +8,9 @@ from math import cos, acos, radians, degrees, sin, sqrt
 
 from models import Car, Smoke
 
-Objects = [Car([500, 500], 30, 15, speed=5)]
+Objects = [Car(1, [500, 500], 30, 15, speed=5, color='#cc0000'), Car(2, [600, 500], 30, 15, speed=5, keys=['w','s','a','d'], color='#0000cc')]
 
-drift_tails = []
+drift_tails = {}
 
 def hyp(vec):
     return sqrt(vec[0]**2 + vec[1]**2)
@@ -30,66 +30,77 @@ def GetCenter(obj):
     return sum(map(lambda x: np.array(x), obj))/len(obj)
 
 def CreateMove():
-    drift_tail_num = 0
+    drift_tail_num = {}
     direction = np.array([0.0, -1.0])
     while True:
-        
-        obj = Objects[0]
-        center = obj.GetBackAxis(obj.map) * 2
-        BackAxis = [center[0] * cos(radians(obj.rotation)) - center[1] * sin(radians(obj.rotation)), center[0] * sin(radians(obj.rotation)) + center[1] * cos(radians(obj.rotation))]
-        obj.vector = Normalize(obj.vector*4*obj.velocity**2 + obj.direction)
+        for obj in Objects:
+            if obj.Type == 'car':
+                obj.vector = Normalize(obj.vector*4*obj.velocity**2 + obj.direction)
+                if keyboard.is_pressed('f5'):
+                    obj.pos = np.array([500, 500], dtype='float64')
+                    obj.x, obj.y = obj.pos
+                # if keyboard.is_pressed(obj.keys[0]) and keyboard.is_pressed(obj.keys[1]): 
+                #     obj.velocity *= 0.96
+                else:
+                    if keyboard.is_pressed(obj.keys[0]):
+                        obj.velocity += (obj.speed - obj.velocity) * 0.01
+                        gas = 1
+                    if keyboard.is_pressed(obj.keys[1]):
+                        obj.velocity += (-obj.speed/3 - obj.velocity) * 0.01
+                        gas = 1
+                if not keyboard.is_pressed(obj.keys[0]) and not keyboard.is_pressed(obj.keys[1]):
+                    gas = 2
+                if keyboard.is_pressed(obj.keys[2]):
+                    obj.rotation -= min(2 * obj.velocity/obj.speed*1.5, 2)
+                    obj.wheel_angle = max(-50 * (abs(obj.velocity)/obj.speed)*1.5, -50)
+                if keyboard.is_pressed(obj.keys[3]):
+                    obj.rotation += min(2 * obj.velocity/obj.speed*1.5, 2)
+                    obj.wheel_angle = min(50 * (abs(obj.velocity)/obj.speed)*1.5, 50)
+                if not keyboard.is_pressed(obj.keys[3]) and not keyboard.is_pressed(obj.keys[2]):
+                    obj.wheel_angle *= 0.9
 
-        if keyboard.is_pressed('up'):
-            obj.velocity += (obj.speed - obj.velocity) * 0.01
-            gas = 1
-        if keyboard.is_pressed('down'):
-            obj.velocity += (-obj.speed - obj.velocity) * 0.01
-            gas = 1
-        if not keyboard.is_pressed('up') and not keyboard.is_pressed('down'):
-            gas = 2
-        if keyboard.is_pressed('left'):
-            obj.rotation -= min(2 * obj.velocity/4, 2)
-        if keyboard.is_pressed('right'):
-            obj.rotation += min(2 * obj.velocity/4, 2)
+                if obj.velocity > 0.001 or obj.velocity < 0.001:
+                    obj.direction = [direction[0] * cos(radians(obj.rotation)) - direction[1] * sin(radians(obj.rotation)), direction[0] * sin(radians(obj.rotation)) + direction[1] * cos(radians(obj.rotation))]
+                    obj.velocity *= 0.995
+                    obj.pos += obj.vector * obj.velocity
+                    obj.x, obj.y = obj.pos
 
-        obj.direction = [direction[0] * cos(radians(obj.rotation)) - direction[1] * sin(radians(obj.rotation)), direction[0] * sin(radians(obj.rotation)) + direction[1] * cos(radians(obj.rotation))]
-        obj.velocity *= 0.995
-        obj.pos += obj.vector * obj.velocity
-        obj.x, obj.y = obj.pos
+                    try:
+                        obj.drift_angle = degrees(acos(np.array(obj.direction).dot(obj.vector) / ((hyp(obj.direction)*hyp(obj.vector)))))
+                    except:
+                        pass
+                    if obj.drift_angle > 25:
+                        drift_tail_num[obj.ID] += 1
+                    else:
+                        drift_tail_num[obj.ID] = 0
 
-        if drift_tail_num % 3 == 0 and drift_tail_num != 0:
-            if drift_tail_num % (6*gas) == 0:
-                Objects.append(Smoke(obj.render[0], obj.vector*obj.velocity))
-                Objects.append(Smoke(obj.render[1], obj.vector*obj.velocity))
-            if drift_tail_num > 3:
-                drift_tails[-1][0].append(obj.render[0])
-                drift_tails[-1][1].append(obj.render[1])
-                drift_tails[-1][2].append(obj.render[2])
-                drift_tails[-1][3].append(obj.render[3])
-            else:
-                drift_tails.append([[] for i in range(4)])
-                drift_tails[-1][0].append(obj.render[0])
-                drift_tails[-1][1].append(obj.render[1])
-                drift_tails[-1][2].append(obj.render[2])
-                drift_tails[-1][3].append(obj.render[3])
-        try:
-            rotate_angle = degrees(acos(np.array(obj.direction).dot(obj.vector) / ((hyp(obj.direction)*hyp(obj.vector)))))
-        except:
-            pass
-        if rotate_angle > 25:
-            drift_tail_num += 1
-        else:
-            drift_tail_num = 0
-        print(rotate_angle)
+                    if drift_tail_num[obj.ID] % 4 == 0 and drift_tail_num[obj.ID] != 0:
+                        if drift_tail_num[obj.ID] % (8*gas) == 0:
+                            Objects.append(Smoke(obj.render[0], obj.vector*obj.velocity))
+                            Objects.append(Smoke(obj.render[1], obj.vector*obj.velocity))
+                        if obj.ID not in drift_tails:
+                            drift_tails[obj.ID] = []
+                        if drift_tail_num[obj.ID] > 4:
+                            drift_tails[obj.ID][-1][0].append(obj.render[0])
+                            drift_tails[obj.ID][-1][1].append(obj.render[1])
+                            drift_tails[obj.ID][-1][2].append(obj.render[2])
+                            drift_tails[obj.ID][-1][3].append(obj.render[3])
+                        else:
+                            drift_tails[obj.ID].append([[] for _ in range(4)])
+                            drift_tails[obj.ID][-1][0].append(obj.render[0])
+                            drift_tails[obj.ID][-1][1].append(obj.render[1])
+                            drift_tails[obj.ID][-1][2].append(obj.render[2])
+                            drift_tails[obj.ID][-1][3].append(obj.render[3])
         time.sleep(1/144)
         
 def Render(canvas):
-    if len(drift_tails) > 2:
-        drift_tails.remove(drift_tails[0])
-    for tails in drift_tails:
-        for tail in tails:
-            if len(tail) > 1:
-                canvas.create_line(*tail, width=3, fill='#444444')
+    for car in drift_tails:
+        if len(drift_tails[car]) > 2:
+            drift_tails[car].remove(drift_tails[car][0])
+        for tails in drift_tails[car]:
+            for tail in tails:
+                if len(tail) > 1:
+                    canvas.create_line(*tail, width=3, fill='#444444')
 
     for obj in Objects:
         obj.Render(canvas)
--- models.py
+++ models.py
@@ -4,12 +4,23 @@ import random
 from math import cos, sin, radians
 
 class Car:
-    def __init__(self, pos, length, width, rotation=0, speed=5) -> None:
+    Type = 'car'
+    def __init__(self, ID, pos, length, width, rotation=0, speed=5, keys=['up', 'down', 'left', 'right'], color='#cc0000') -> None:
+        self.ID = ID
+        self.keys = keys
+        self.color = color
         self.x, self.y = pos
         self.pos = np.array(pos, dtype='float64')
 
         self.length = length
         self.width = width
+
+        self.wheels = [
+            Wheel(np.array([-self.width/2, self.length/2-5]), True),
+            Wheel(np.array([self.width/2, self.length/2-5]), True),
+            Wheel(np.array([-self.width/2, -self.length/2+5]), False),
+            Wheel(np.array([self.width/2, -self.length/2+5]), False)
+        ]
 
         points = [
                 [0, 0],
@@ -24,17 +35,21 @@ class Car:
         self.vector = np.array([0.0, 0.0])
         self.direction = np.array([0.0, -1.0])
         self.velocity = 0.0
+        self.wheel_angle = 0.0
+        self.drift_angle = 0.0
 
         self.map = points
         self.render = list(map(lambda point: [self.x + point[0], self.y + point[1]], self.map))
         self.lines = self.GetLines(self.render)
     
     def Render(self, canvas):
+        for wheel in self.wheels:
+            wheel.Render(canvas, self.GetCenter(self.GetMap()), [min(self.wheel_angle, 45.0), self.rotation], self.drift_angle)
         render = self.GetMap()
         center = self.GetCenter(render)
         back = self.GetBackAxis(render)
         self.render = list(map(lambda line: [(back[0] - line[0]) * cos(radians(self.rotation)) - (back[1] - line[1]) * sin(radians(self.rotation)) + center[0], (back[0] - line[0]) * sin(radians(self.rotation)) + (back[1] - line[1]) * cos(radians(self.rotation)) + center[1]], render))
-        canvas.create_polygon(*self.render, fill='#ff0000')
+        canvas.create_polygon(*self.render, fill=self.color)
 
     def GetLines(self, obj):
         return [[obj[i-1], obj[i]] for i in range(-1, len(obj)-1)]
@@ -47,15 +62,18 @@ class Car:
     
     def GetMap(self):
         return list(map(lambda point: [self.x + point[0], self.y + point[1]], self.map))
+
     
 class Smoke:
+    Type = 'smoke'
     def __init__(self, pos, vector=[0,0]) -> None:
-        self.size = random.randint(5, 11)
+        self.size = random.randint(3, 10)
         self.pos = np.array(pos)
         self.vector = np.array(vector)
         self.rotation = random.randint(-45, 45)
         self.rotation_velocity = random.randint(-5, 5)
         self.size_transform_velocity = random.randint(1, 2)
+        self.color_change_velocity = random.randint(3,5)
         self.color = 102
         self.alive = True
 
@@ -65,7 +83,7 @@ class Smoke:
             self.pos += self.vector*0.6
             self.x, self.y = self.pos
             self.rotation += self.rotation_velocity
-            self.color += 3
+            self.color += self.color_change_velocity
             self.map = [
                     [0, 0],
                     [self.size, 0],
@@ -76,7 +94,7 @@ class Smoke:
             center = self.GetCenter(render)
             self.render = list(map(lambda line: [(center[0] - line[0]) * cos(radians(self.rotation)) - (center[1] - line[1]) * sin(radians(self.rotation)) + center[0], (center[0] - line[0]) * sin(radians(self.rotation)) + (center[1] - line[1]) * cos(radians(self.rotation)) + center[1]], render))
             canvas.create_polygon(*self.render, fill=f'#{hex(self.color).split("0x")[1] * 3}')
-            if hex(self.color) == '0xcc':
+            if self.color > 204:
                 self.alive = False
     
     def GetCenter(self, obj):
@@ -84,4 +102,39 @@ class Smoke:
 
     def GetMap(self):
         return list(map(lambda point: [self.x + point[0], self.y + point[1]], self.map))
+
+
+class Wheel:
+    Type = 'wheel'
+    def __init__(self, offset, rotating=False) -> None:
+        self.offset = offset
+        self.rotating = rotating
+        self.height = 8
+        self.width = 4
+        self.map = [
+                    [0, 0],
+                    [self.width, 0],
+                    [self.width, self.height],
+                    [0, self.height]
+                    ]
+
+    def Render(self, canvas, pos, rotation, drift_angle):
+        self.x, self.y = self.pos = self.x, self.y = self.pos = pos + self.offset - [self.width/2, -self.height*1.4]
+        self.rotation = rotation
+        center = pos
+        render = self.GetMap()
+        self.render = list(map(lambda line: [(center[0] - line[0]) * cos(radians(self.rotation[1])) - (center[1] - line[1]) * sin(radians(self.rotation[1])) + center[0], (center[0] - line[0]) * sin(radians(self.rotation[1])) + (center[1] - line[1]) * cos(radians(self.rotation[1])) + center[1]], render))
+        if self.rotating:
+            mp = cos(radians(min(drift_angle, 50)/25*90))
+            self.rotation[0] *= mp
+            center = self.GetCenter(self.render)
+            self.render = list(map(lambda line: [(center[0] - line[0]) * cos(radians(self.rotation[0])) - (center[1] - line[1]) * sin(radians(self.rotation[0])) + center[0], (center[0] - line[0]) * sin(radians(self.rotation[0])) + (center[1] - line[1]) * cos(radians(self.rotation[0])) + center[1]], self.render))
+        canvas.create_polygon(*self.render, fill='#111111')
+
+    def GetCenter(self, obj):
+        return sum(map(lambda x: np.array(x), obj))/len(obj)
+
+    def GetMap(self):
+        return list(map(lambda point: [self.x + point[0], self.y + point[1]], self.map))
+
     
